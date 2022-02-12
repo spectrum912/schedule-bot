72620747463b83f4a027ca4a7c494b9e383f9655
sns23 <nikita.solodovnik@gmail.com>
2022-02-12 15:33:01 +0200

    init

---
+++ file/main.py
@@ -0,0 +1,16 @@
+import requests
+
+API_LINK = "https://api.telegram.org/bot5167627424:AAFGOxsZa3YH4egdKF2FjDBsrsbh37rhqRs"
+
+updates = requests.get(API_LINK + "/getUpdates?offset=-1").json()
+
+print(updates)
+
+#get a meassage
+message = updates['result'][0]['message']
+chat_id = message['from']['id']
+text = message['text']
+
+sent_message = requests.get(API_LINK + f'/sendMessage?chat_id={chat_id}&text= ТУТ БУДЕТ РАССПИСАНИЕ')
+
+
